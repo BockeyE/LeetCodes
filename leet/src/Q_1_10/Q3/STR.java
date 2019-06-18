e43debdda7d9d6abf5999288f32ce96c625e72a5
b <gymnopediece@163.com>
2019-06-18 15:36:56 +0800

    leet Q3

--- leet/src/Q_1_10/Q3/STR.java
+++ leet/src/Q_1_10/Q3/STR.java
@@ -58,4 +58,20 @@ public class STR {
         int acdacd = STR.lengthOfLongestSubstring("tmmzuxt");
         System.out.println(acdacd);
     }
+
+
+    /**
+     *  解题思路。比较直接。
+     *  首先准备一个hashset，在python中用set，golang中直接用map。
+     *  然后遍历字符串，判断是否存在在set中，如果已经存在，说明当前字符已经出现了
+     *  那么这个时候的操作是最关键的。
+     *  要从计算长度的起始点开始，即left++，逐个判断left位置的字符，是不是当前出现的，重复过的字符
+     *  如果不是，就要从set中删掉，因为left增加，最长长度永远从left开始算，那么left位置之前的字符不能存在在set中
+     *  否则会算错。所以必须逐个判断 left位置是否是当前重复元素
+     *  直到找到重复的元素位置，这个时候，left++一次即可跳出这个检验循环，此时，left位置是刚刚重复过的元素，第一次出现位置的后一个位置。
+     *
+     *  然后在计算长度，保留max即可，通过这思路就可以直接找到最长str
+     *
+     *
+     */
 }
