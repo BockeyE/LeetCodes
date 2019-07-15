f77fdc3ab707bfc72aa96b368e7177bb78b1a826
b <gymnopediece@163.com>
2019-07-15 18:49:09 +0800

    leet Q3

---
+++ leet/src/Q_1_10/Q4/mid.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        A = nums1
+        B = nums2
+
+        if len(nums1) > len(nums2):
+            B = nums1
+            A = nums2
+
+        m = len(A)
+        n = len(B)
+
+        iMin = 0
+        iMax = m
+
+        halfLen = (m + n + 1) / 2
+        while iMin <= iMax:
+            i = (iMin + iMax) / 2
+            j = halfLen - i
+            if i < iMax and B[int(j - 1)] > A[int(i)]:
+                iMin = i + 1
+            elif i > iMin and A[int(i - 1)] > B[int(j)]:
+                iMax = i - 1
+            else:
+                maxLeft = 0
+                if i == 0:
+                    maxLeft = B[int(j - 1)]
+                elif j == 0:
+                    maxLeft = A[int(i - 1)]
+                else:
+                    maxLeft = float(max(B[int(j - 1)], A[int(i - 1)]))
+                if (m + n) % 2 == 1:
+                    return maxLeft
+
+                minRight = 0
+                if i == m:
+                    minRight = B[int(j)]
+                elif j == n:
+                    minRight = A[int(i)]
+                else:
+                    minRight = min(B[int(j)], A[int(i)])
+                minRight = float(minRight)
+                maxLeft = float(maxLeft)
+                return float((maxLeft + minRight) / 2)
