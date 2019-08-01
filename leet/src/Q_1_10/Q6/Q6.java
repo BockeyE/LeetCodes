4f72ba7d7fa44988b469889d3a9af09110457abb
b <gymnopediece@163.com>
2019-08-01 15:05:47 +0800

    leet Q6

---
+++ leet/src/Q_1_10/Q6/Q6.java
@@ -0,0 +1,31 @@
+package Q_1_10.Q6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author bockey
+ */
+public class Q6 {
+    public String convert(String s, int numRows) {
+        List arrlist = new ArrayList();
+        for (int i = 1; i <= numRows; i++) {
+            ArrayList arr = new ArrayList();
+            arrlist.add(arr);
+        }
+        char[] chars = s.toCharArray();
+        char[] ret=new char[chars.length];
+
+
+
+
+
+        return String.copyValueOf(ret);
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(('c'));
+    }
+}
