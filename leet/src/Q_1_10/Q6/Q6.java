272c6c6cf12389aa5fced676ee498ad8e3a723f5
b <gymnopediece@163.com>
2019-08-02 17:53:23 +0800

    leet Q6

--- leet/src/Q_1_10/Q6/Q6.java
+++ leet/src/Q_1_10/Q6/Q6.java
@@ -1,6 +1,7 @@
 package Q_1_10.Q6;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,24 +9,54 @@ import java.util.List;
  */
 public class Q6 {
     public String convert(String s, int numRows) {
-        List arrlist = new ArrayList();
+        List<ArrayList> arrlist = new ArrayList<>();
         for (int i = 1; i <= numRows; i++) {
             ArrayList arr = new ArrayList();
             arrlist.add(arr);
         }
         char[] chars = s.toCharArray();
-        char[] ret=new char[chars.length];
+        ArrayList ret = new ArrayList();
+        int i = 0;
+        int period = numRows * 2;
+        while (i < chars.length) {
+            if (i % period == 0) {
+                for (int j = 0; j < numRows; ) {
+                    System.out.println("position i : " + i + " >> value: " + chars[i]);
+                    arrlist.get(j).add(chars[i]);
+                    j++;
+                    i++;
+                }
+                for (int j = numRows - 2; j > 0; ) {
+                    System.out.println("position i : " + i + " >> value: " + chars[i]);
+                    arrlist.get(j).add(chars[i]);
+                    i++;
+                    j--;
+                }
+            }
+            i++;
 
-
-
-
-
-        return String.copyValueOf(ret);
+        }
+        System.out.println(arrlist);
+        for (ArrayList arrayList : arrlist) {
+            for (Object o : arrayList) {
+                for (Object o1 : arrayList) {
+                    ret.add(o1);
+                }
+            }
+        }
+        char[] rets = s.toCharArray();
+        for (int m = 0; m < rets.length; m++) {
+            rets[m] = (char) ret.get(m);
+        }
+        return String.copyValueOf(rets);
 
 
     }
 
     public static void main(String[] args) {
-        System.out.println(('c'));
+        Q6 q = new Q6();
+        String s = q.convert("LEETCODEISHIRING", 4);
+//        LDREOEIIECIHNTSG
+        System.out.println(s);
     }
 }
