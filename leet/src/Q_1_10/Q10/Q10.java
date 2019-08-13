d0e5099940255e184429bd0cbbc7fb31399fb1f5
b <gymnopediece@163.com>
2019-08-13 11:18:04 +0800

    leet Q3

--- leet/src/Q_1_10/Q10/Q10.java
+++ leet/src/Q_1_10/Q10/Q10.java
@@ -4,52 +4,30 @@ package Q_1_10.Q10;
  * @author bockey
  */
 public class Q10 {
-    public boolean isMatch(String s, String p) {
-        if ((!p.contains("*") && !p.contains("."))) {
-            return s.equals(p);
-        }
+    public boolean isMatch(String s, String pattern) {
 
-        int sp = 0;
-        int pp = 0;
-        while (sp < s.length() && pp < p.length()) {
-            if (s.charAt(sp) == p.charAt(pp) || p.charAt(pp) == '.') {
-                sp++;
-                pp++;
-                continue;
-            }
-            if (s.charAt(sp) != p.charAt(pp)) {
-                if (p.charAt(pp) == '*') {
-                    sp--;
-                    if (p.charAt(pp - 1) == '.') {
-                        if (pp + 1 >= p.length() - 1) {
-                            return true;
-                        } else {
-                            while (sp < s.length()) {
-                                if (p.charAt(pp + 1) == s.charAt(sp)) {
-                                    break;
-                                }
-                                sp++;
-                            }
-                        }
-                    }
-                    while (s.charAt(sp) == p.charAt(pp - 1) && sp < s.length() - 1) {
-                        sp++;
-                    }
-                    if (sp >= s.length() - 1) {
-                        return true;
-                    }
-                    pp++;
-                    continue;
-                }
-                if ((pp < p.length() - 1) && p.charAt(pp + 1) == '*') {
-                    pp = pp + 2;
-                    continue;
-                } else {
-                    return false;
-                }
-            }
+//        如果模式串中有星号，它会出现在第二个位置，
+//        即 \text{pattern[1]}pattern[1] 。
+//        这种情况下，我们可以直接忽略模式串中这一部分，
+//        或者删除匹配串的第一个字符，前提是它能够匹配模式串当前位置字符，
+//        即 \text{pattern[0]}pattern[0] 。
+//        如果两种操作中有任何一种使得剩下的字符串能匹配，
+//        那么初始时，匹配串和模式串就可以被匹配。
+
+
+//        先判断是否为空，
+        if (pattern.isEmpty()) return s.isEmpty();
+//        第一步匹配结果，不为空且首位字符匹配
+        boolean first_match = (!s.isEmpty() &&
+                (pattern.charAt(0) == s.charAt(0) || pattern.charAt(0) == '.'));
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
+            return (isMatch(s, pattern.substring(2)) ||
+                    //        后续匹配结果，长度2以上，且为*时，直接递归匹配，以第三个字符开始的pattern
+
+                    (first_match && isMatch(s.substring(1), pattern)));
+        } else {
+            return first_match && isMatch(s.substring(1), pattern.substring(1));
         }
-        return sp >= s.length() && pp >= p.length();
     }
 
     public static void main(String[] args) {
