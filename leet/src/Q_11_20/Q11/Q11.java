2f91e8aaae139064b79816965f1d51d07c3f1cba
b <gymnopediece@163.com>
2019-08-14 17:00:04 +0800

    leet Q3

---
+++ leet/src/Q_11_20/Q11/Q11.java
@@ -0,0 +1,35 @@
+package Q_11_20.Q11;
+
+/**
+ * @author bockey
+ */
+public class Q11 {
+//    执行用时 : 4 ms, 在所有 Java 提交中击败了95.71%    的用户
+//    内存消耗 : 44.9 MB, 在所有 Java 提交中击败了58.71%    的用户
+
+    public int maxArea(int[] height) {
+//直接使用双指针法
+        int start = 0;
+        int end = height.length - 1;
+        int tem1 = 0;
+        boolean act = true;
+        int max = 0;
+        while (start != end) {
+            tem1 = ((height[start] <= height[end]) ? height[start] : height[end]) * (end - start);
+            max = (tem1 >= max) ? tem1 : max;
+            if ((height[start] <= height[end])) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+//        int[] a = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+//        int[] a = {2, 3, 10, 5, 7, 8, 9};
+        int[] a = {1, 1};
+        System.out.println(new Q11().maxArea(a));
+    }
+}
