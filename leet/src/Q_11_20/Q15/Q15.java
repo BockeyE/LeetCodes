26ce4fe3ffd9430d5b82eddbb537b0639b1d6e7f
b <gymnopediece@163.com>
2019-08-23 16:54:28 +0800

    leet Q15

---
+++ leet/src/Q_11_20/Q15/Q15.java
@@ -0,0 +1,23 @@
+package Q_11_20.Q15;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author bockey
+ */
+public class Q15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        ArrayList<Integer> ar = Arrays.asList();
+
+    }
+
+    public static void main(String[] args) {
+        int[] ss = {1, 2, 3, 4};
+        System.out.println(new Q15().threeSum(ss));
+    }
+}
+}
