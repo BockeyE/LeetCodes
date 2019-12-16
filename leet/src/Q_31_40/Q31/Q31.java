5481bb215d4fb9c137d06f398ec16afeefd5c85b
b <gymnopediece@163.com>
2019-12-16 18:36:34 +0800

    q15

--- leet/src/Q_31_40/Q31/Q31.java
+++ leet/src/Q_31_40/Q31/Q31.java
@@ -3,6 +3,9 @@ package Q_31_40.Q31;
 /**
  * @author bockey
  * 我们需要找到给定数字列表的下一个字典排列，而不是由给定数组形成的数字。
+ *
+ * 执行用时 : * 1 ms * , 在所有 java 提交中击败了 * 100.00% * 的用户
+ * 内存消耗 : * 37.8 MB * , 在所有 java 提交中击败了 * 85.69% * 的用户
  */
 public class Q31 {
     public void nextPermutation(int[] nums) {
