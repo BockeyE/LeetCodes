eda8e0c429c3e87a2b3ed2f7996b8bc56883ed33
b <gymnopediece@163.com>
2020-03-09 17:26:23 +0800

    q38

---
+++ leet/src/Q_31_40/Q38/Q38.java
@@ -0,0 +1,67 @@
+package Q_31_40.Q38;
+
+/**
+ * @author bockey
+ * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。前五项如下：
+ * <p>
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ * 6.     312211
+ * 7.     13112221
+ * 1 被读作  "one 1"  ("一个一") , 即 11。
+ * 11 被读作 "two 1s" ("两个一"）, 即 21。
+ * 21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+ * <p>
+ * 给定一个正整数 n（1 ≤ n ≤ 30），输出外观数列的第 n 项。
+ */
+public class Q38 {
+    /**
+     * 思路， 遍历当前str，设置2个标，一个记录当前起始点，一个往右走，如果当前右点字符与起始点不一样，则记录数量，并拼接成新的
+     * 此时将起始点设置为右点，继续至str结束
+     *
+     * @param n
+     * @return
+     */
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        String tar = "1";
+        int k = 1;
+        while (k < n) {
+            tar = generateNext(tar);
+            k++;
+        }
+        return tar;
+    }
+
+    public String generateNext(String tar) {
+        if ("1".equals(tar)) {
+            return "11";
+        }
+        if ("11".equals(tar)) {
+            return "21";
+        }
+        StringBuilder sb = new StringBuilder();
+        int m = 0;
+        int count = 0;
+        for (int i = 0; i < tar.length(); ) {
+            m = i;
+            while (m < tar.length() && tar.charAt(m) == tar.charAt(i)) {
+                m++;
+                count++;
+            }
+            sb.append(count).append(tar.charAt(i));
+            i = m;
+            count = 0;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Q38().countAndSay(5));
+    }
+}
